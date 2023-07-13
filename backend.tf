281fc99770c088d2139592b83f9b7e035374fe3b
lily <lethanhpphuongvo.com>
2023-07-13 11:28:33 +1000

    Delete the assume role

--- backend.tf
+++ backend.tf
@@ -1,24 +1,15 @@
 provider "aws" {
     region = "ap-southest-2"
-
-    assume_role{
-        role_arn: "arn:aws:iam::252874068638:role/GitHubAction-AssumeRoleWithAction"
-        session_name: "tf-lilyvo-cloud resume-challenge"
-        external_id: "LeeVo2408@github"
-    }
-    # }
-    # # default_tags{
-    # #     # tags: 
-    # }
 }
+
 terraform {
     required_version = ">= 1.0.0, < 2.0.0"
     
     required_providers{
         aws = {
-            source = "hashicorp/aws"
-            version = " -> 4.8"
-        }
+          source = "hashicorp/aws"
+          version = " -> 4.8"
+          }
 
     }
     
@@ -27,9 +18,6 @@ terraform {
         key = "terraform.tfstate"
         region: "ap-southeast-2"
         encrypt = true
-        #dynamodb_table = "terraform-locks"
-        #role_arn = "arn:aws:iam::252874068638:role/GitHubAction-AssumeRoleWithAction"
-    
     }
         
 }
