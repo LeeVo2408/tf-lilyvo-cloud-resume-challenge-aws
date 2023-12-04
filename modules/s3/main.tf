33f6288f229c9e927620b820065076a7fd6319c4
Phương Võ <phuongvo@Phuongs-MacBook-Air-2.local>
2023-12-04 11:35:21 +1100

    copy code from grassi

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,7 +1,7 @@
 #tfsec:ignore:aws-s3-enable-versioning
 #tfsec:ignore:aws-s3-encryption-customer-key
 resource "aws_s3_bucket" "cloud_resume_site_bucket" {
-  bucket = "tf-aws-lilyvo-cloud-resume-challenge-${var.environment}-site"
+  bucket = "tf-aws-mgrassi-cloud-resume-challenge-${var.environment}-site"
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "cloud_resume_site_bucket" {
@@ -15,6 +15,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "cloud_resume_site
     }
   }
 }
+
 resource "aws_s3_bucket_public_access_block" "cloud_resume_site_bucket" {
   bucket = aws_s3_bucket.cloud_resume_site_bucket.id
 
@@ -29,6 +30,7 @@ resource "aws_s3_object" "index" {
   key    = "index.html"
   source = "src/index.html"
   etag = filemd5("src/index.html")
+  content_type = "text/html"
 }
 
 resource "aws_s3_object" "error" {
@@ -36,24 +38,18 @@ resource "aws_s3_object" "error" {
   key    = "error.html"
   source = "src/error.html"
   etag = filemd5("src/error.html")
+  content_type = "text/html"
 }
 
-resource "aws_s3_bucket_website_configuration" "cloud_resume_site_bucket" {
+resource "aws_s3_bucket_policy" "cloud_resume_site_bucket" {
   bucket = aws_s3_bucket.cloud_resume_site_bucket.id
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
+  policy = data.aws_iam_policy_document.cloud_resume_site_bucket.json
 }
 
 #tfsec:ignore:aws-s3-enable-bucket-logging
 #tfsec:ignore:aws-s3-encryption-customer-key
 resource "aws_s3_bucket" "cloud_resume_logging_bucket" {
-  bucket = "tf-aws-lilyvo-cloud-resume-challenge-${var.environment}-logging"
+  bucket = "tf-aws-mgrassi-cloud-resume-challenge-${var.environment}-logging"
 }
 
 resource "aws_s3_bucket_versioning" "cloud_resume_logging_bucket" {
@@ -104,3 +100,110 @@ resource "aws_s3_bucket_logging" "cloud_resume_logging_bucket" {
   target_bucket = aws_s3_bucket.cloud_resume_logging_bucket.id
   target_prefix = "log/"
 }
+
+# #tfsec:ignore:aws-s3-enable-versioning
+# #tfsec:ignore:aws-s3-encryption-customer-key
+# resource "aws_s3_bucket" "cloud_resume_site_bucket" {
+#   bucket = "tf-aws-lilyvo-cloud-resume-challenge-${var.environment}-site"
+# }
+
+# resource "aws_s3_bucket_server_side_encryption_configuration" "cloud_resume_site_bucket" {
+#   bucket = aws_s3_bucket.cloud_resume_site_bucket.id
+
+#   rule {
+#     bucket_key_enabled = true
+    
+#     apply_server_side_encryption_by_default {
+#       sse_algorithm = "AES256"
+#     }
+#   }
+# }
+# resource "aws_s3_bucket_public_access_block" "cloud_resume_site_bucket" {
+#   bucket = aws_s3_bucket.cloud_resume_site_bucket.id
+
+#   block_public_acls       = true
+#   block_public_policy     = true
+#   ignore_public_acls      = true
+#   restrict_public_buckets = true
+# }
+
+# resource "aws_s3_object" "index" {
+#   bucket = aws_s3_bucket.cloud_resume_site_bucket.id
+#   key    = "index.html"
+#   source = "src/index.html"
+#   etag = filemd5("src/index.html")
+# }
+
+# resource "aws_s3_object" "error" {
+#   bucket = aws_s3_bucket.cloud_resume_site_bucket.id
+#   key    = "error.html"
+#   source = "src/error.html"
+#   etag = filemd5("src/error.html")
+# }
+
+# resource "aws_s3_bucket_website_configuration" "cloud_resume_site_bucket" {
+#   bucket = aws_s3_bucket.cloud_resume_site_bucket.id
+
+#   index_document {
+#     suffix = "index.html"
+#   }
+
+#   error_document {
+#     key = "error.html"
+#   }
+# }
+
+# #tfsec:ignore:aws-s3-enable-bucket-logging
+# #tfsec:ignore:aws-s3-encryption-customer-key
+# resource "aws_s3_bucket" "cloud_resume_logging_bucket" {
+#   bucket = "tf-aws-lilyvo-cloud-resume-challenge-${var.environment}-logging"
+# }
+
+# resource "aws_s3_bucket_versioning" "cloud_resume_logging_bucket" {
+#   bucket = aws_s3_bucket.cloud_resume_logging_bucket.id
+
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
+
+# resource "aws_s3_bucket_server_side_encryption_configuration" "cloud_resume_logging_bucket" {
+#   bucket = aws_s3_bucket.cloud_resume_logging_bucket.id
+
+#   rule {
+#     bucket_key_enabled = true
+
+#     apply_server_side_encryption_by_default {
+#       sse_algorithm = "AES256"
+#     }
+#   }
+# }
+
+# resource "aws_s3_bucket_public_access_block" "cloud_resume_logging_bucket" {
+#   bucket = aws_s3_bucket.cloud_resume_logging_bucket.id
+
+#   block_public_acls       = true
+#   block_public_policy     = true
+#   ignore_public_acls      = true
+#   restrict_public_buckets = true
+# }
+
+# resource "aws_s3_bucket_ownership_controls" "cloud_resume_logging_bucket" {
+#   bucket = aws_s3_bucket.cloud_resume_logging_bucket.id
+
+#   rule {
+#     object_ownership = "BucketOwnerPreferred"
+#   }
+# }
+
+# resource "aws_s3_bucket_acl" "cloud_resume_logging_bucket" {
+#   bucket = aws_s3_bucket.cloud_resume_logging_bucket.id
+#   acl    = "log-delivery-write"
+# }
+
+# resource "aws_s3_bucket_logging" "cloud_resume_logging_bucket" {
+#   bucket = aws_s3_bucket.cloud_resume_site_bucket.id
+
+#   target_bucket = aws_s3_bucket.cloud_resume_logging_bucket.id
+#   target_prefix = "log/"
+# }
