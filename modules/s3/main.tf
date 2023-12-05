e5575fbf2a08d8032b33dc4d79adc4ee376f1b78
Phương Võ <phuongvo@Phuongs-MacBook-Air-2.local>
2023-12-05 14:24:18 +1100

    change the content type

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -31,6 +31,7 @@ resource "aws_s3_object" "index" {
   key    = "index.html"
   source = "src/index.html"
   etag = filemd5("src/index.html")
+  content_type = "text/html"
 }
 
 resource "aws_s3_object" "error" {
